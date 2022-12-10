b5c22245d54e7c3e4adabb45bdbfc006b9f61bff
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-09 16:02:59 -0800

    calculated points matrix

--- rock_paper_scissors.cpp
+++ rock_paper_scissors.cpp
@@ -3,5 +3,88 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+  const int ROCK = 0, PAPER = 1, SCISSORS = 2;
+  char opponent_choice, my_choice;
+  // A = Rock       - 1 Point
+  // B = Paper      - 2 Points
+  // C = Scissors   - 3 Points
+
+  int choice[3] = {1, 2, 3};
+
+  // Rock 0
+  // Paper 1
+  // Scissors 2
+  int defeats[3];
+
+  defeats[ROCK] = SCISSORS;
+  defeats[PAPER] = ROCK;
+  defeats[SCISSORS] = PAPER;
+
+  // Lost - 0 Points
+  // Draw - 3 Points
+  // Won  - 6 Points
+
+  int points_matrix[3][3] = {
+      // rock, paper, scissors
+      {1, 1, 1},  // rock
+      {2, 2, 2},  // paper
+      {3, 3, 3}   // scissors
+  };
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      points_matrix[i][j] = choice[i];
+      if (i == j) {
+        // Tie
+        points_matrix[i][j] += 3;
+      } else {
+        if (defeats[i] == j) {
+          // won 6 points
+          points_matrix[i][j] += 6;
+
+        } else if (defeats[j] == i) {
+          // lost 0 points
+        }
+      }
+
+      cout << points_matrix[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+  exit(0);
+
+  // read input
+  while (!cin.eof()) {
+    cin >> opponent_choice;
+    cin >> my_choice;
+
+    switch (opponent_choice) {
+      case 'A':
+        cout << "A";
+        break;
+
+      case 'B':
+        cout << "B";
+        break;
+
+      case 'C':
+        cout << "C";
+        break;
+
+      default:
+        cout << "Error in input";
+        exit(0);
+    }
+
+    cin.get();             // reads in the new line character \n after the input (on the same line)
+    char c2 = cin.peek();  // reads first character of the next line
+
+    if ((c2 == '\n') || (cin.eof())) {
+      // end of this elf
+      // cout << "total calories: " << total_calories << endl;
+    }
+  }
+
   return 0;
 }
